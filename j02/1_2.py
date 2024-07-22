611d73a24dd8d8a9ade5ef01743dd3f3ff3efe4a
nafas-2024 <nafas.hashemzade23@gmail.com>
2024-07-22 15:40:44 +0330

    Add files via upload

---
+++ j02/1_2.py
@@ -0,0 +1,7 @@
+num1=int(input("enter a number: "))
+num2=int(input("enter another Number: "))
+print("sum is:",num1+num2)
+print("subtraction is: ",num1-num2)
+print("multiplication is: ",num1*num2)
+print("division is: ",num1/num2)
+
---
+++ j02/2_2.py
@@ -0,0 +1,4 @@
+num1=int(input("enter number1: "))
+num2=int(input("enter number2: "))
+num3=int(input("enter number3: "))
+print((num1+num2+num3)/3)
