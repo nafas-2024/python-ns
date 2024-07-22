ba13eb471efd563cafc1c66146f9a89af2f3d3ea
nafas-2024 <nafas.hashemzade23@gmail.com>
2024-07-22 15:44:13 +0330

    Delete 5.py

--- 5.py
+++
@@ -1,8 +0,0 @@
-a=int(input("yek adad do raghami vared konid :"))
-
-n1=a%10
-n2=a//10
-
-print("reverse=",n1,n2,sep="")
-print("sum=",n1+n2)
-print("sum the reverse and the first number=",(int(str(n1)+str(n2))+a))
