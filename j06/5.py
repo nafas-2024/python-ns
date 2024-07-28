6ca43a421b0ce9be0b014f1666e279061fd6d3f5
nafas-2024 <nafas.hashemzade23@gmail.com>
2024-07-28 12:07:50 +0330

    Update 5.py

--- j06/5.py
+++ j06/5.py
@@ -1,6 +1,6 @@
-tup=("6")
+tup=("6")        
 
-tup=list(tup)
-tup[0]="python"
+tup=list(tup)     #تاپل را تبدیل به لیست کن
+tup[0]="python"    #کلمه ی پایتون را سیو کن tupداخل عضو صفرم لیست 
 
-print(tuple(tup))
+print(tuple(tup))  # را چاپ کن tup تاپل شده ی لیست 
