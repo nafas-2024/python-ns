3c4998966f44f99af62f68b45271fbe9020bf732
nafas-2024 <nafas.hashemzade23@gmail.com>
2024-07-28 13:02:54 +0330

    Update 8.py

--- j06/8.py
+++ j06/8.py
@@ -2,9 +2,9 @@ l1=[]
 l2=[]
 
 for i in range(1,101):
-    if i % 2 ==0 :
-        l1.append(i)
-    elif i % 2 !=0 :
-        l2.append(i)
+    if i % 2 ==0 :                 #اگر اعداد بین یک تا 100 زوج بود
+        l1.append(i)               #آن را به آخر لیست 1اضافه کن
+    else :
+        l2.append(i)              #در غیر این صورت به آخر لیست 2 اضافه کن
 print("Even numbers from 1 to 100 : " , l1 , end="\n\n")
 print("Odd numbers from 1 to 100" , l2 , end="\n")
