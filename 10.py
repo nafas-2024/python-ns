66af8d09070e916309ed03832c45246de7744213
nafas-2024 <nafas.hashemzade23@gmail.com>
2024-08-04 14:55:31 +0330

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,5 +1,5 @@
-def difit(a:int,b:int=0):
-    if a<10:
+def difit(a,b=0):
+    if a<10:               #را بر گردان a اگر عدد اول از 10 کوچک تر بود 
         return a+b
     
     return difit(a//10,a%10+b)
