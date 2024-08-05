cca1857da5fa7a8487eeb2f1ce865e0898dbd944
nafas-2024 <nafas.hashemzade23@gmail.com>
2024-08-05 21:08:01 +0330

    Delete 10.py

--- 10.py
+++
@@ -1,8 +0,0 @@
-def difit(a,b=0):
-    if a<10:               #را بر گردان a اگر عدد اول از 10 کوچک تر بود 
-        return a+b
-    
-    return difit(a//10,a%10+b)
-
-n=int(input())
-print((difit(difit(difit(n)))))
