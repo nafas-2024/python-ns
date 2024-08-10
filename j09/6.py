f6168315536d68abf4dec6b95dbb580fa1a20660
nafas-2024 <nafas.hashemzade23@gmail.com>
2024-08-10 07:53:57 +0330

    Add files via upload

---
+++ j09/6.py
@@ -0,0 +1,18 @@
+def tas(x=int(input())):                #تابعی که عددی را از کاربر میگیرد
+   
+    if x==1 :
+        print(6)
+    elif x==2 :
+        print(5)
+    elif x==3 :
+        print(4)                     #اگر کاربر عدد 1تا 6 را وارد کرد اعداد داده شده را چاپ کن 
+    elif x==4 :
+        print(3)
+    elif x==5 :
+        print (2)
+    elif x==6:
+        print(1)
+    else :
+        print("not correct!")
+        
+tas()
