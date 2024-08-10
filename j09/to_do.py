db2d38fac727b6359573aa96c9ea487e4dcdec8f
nafas-2024 <nafas.hashemzade23@gmail.com>
2024-08-10 07:52:24 +0330

    Update to_do.py

--- j09/to_do.py
+++ j09/to_do.py
@@ -7,26 +7,26 @@ def to_do_list(works):
             "enter \"done\"to mark a task as done",
             "enter \"remove\"to remove a task",
             "enter \"exit\"to exit the program",sep="\n")
-        work=input("input : ")
+        work=input("input : ")   #داده ای را از کاربر میگیرد
         
         if work=="add":
             adds=input("enter a new task : ")
-            lst.append(adds)
+            lst.append(adds)               #  را تبدیل به لیست کن adds بود  add اگر داده برابر با    
         if work=="view":
             print(lst)
         if work=="done":
             dones=int(input("enter the number of your program you done and start by 0 : "))
             
             lst.insert(dones,"*")
-            print(lst)
+            print(lst)                           #  بود به پشت شماره ی عدد وارد شده * اضافه کن done اگر داده برابر با 
         if work=="remove":
             n=int(input("enter the number do you want to delete and start by 0 : "))
             lst.pop(n)
-            print(lst)
+            print(lst)                       # بود به ازای عددی که کاربر وارد می کند ان را حذف کن remove  اگر داده برابر با 
         if work=="exit":
             print("end")
-            break
+            break                          # بود حلقه را بشکن exit اگر دهده برابر با 
         
         
-works=input()
+works=""
 to_do_list(works)
