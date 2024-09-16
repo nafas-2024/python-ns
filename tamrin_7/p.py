dff9904c0f6a2eacafa2f4bd9adfd139cf8ea424
nafas-2024 <nafas.hashemzade23@gmail.com>
2024-09-16 14:39:39 +0330

    Delete tamrin_7/p.py

--- tamrin_7/p.py
+++
@@ -1,9 +0,0 @@
-def printer(inp):
-    """this is a function that takes an intiger 
-    and prints each digit in range of that digit
-    """
-    for i in inp:
-        print(i ,":", i * int(i))
-    exit()
-inp=input()
-print(printer(inp))
